57a4f9f8d6ef5fabc791ff1a8e34ea7a6cb75338
Ignacio Olave <olave.ia@icloud.com>
2022-09-28 22:25:04 -0300

    First attemp of a snake generator

---
+++ test/generators/snake.c
@@ -0,0 +1,16 @@
+#include <snake.h>
+
+struct Snake generateSingleNodeSnake(void) {
+    return NULL;
+}
+
+struct Snake generateMultipleNodesSnake(int qty) {
+    int i;
+    
+    if (qty <= 0) return NULL;
+    if (qty == 1) return generateSingleNodeSnake();
+
+
+
+    return NULL;
+}
